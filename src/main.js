56fb40ccdab99fea55a8d636d1b6e825c4afaf3b
douglasblnk <douglasblank1@gmail.com>
2019-10-30 21:36:22 -0300

    Started the new project

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './routes'
+import Fontawesome from '@fortawesome/fontawesome-free/css/all.css'
+import BootstrapVue from 'bootstrap-vue';
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(Fontawesome);
+Vue.use(BootstrapVue);
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  beforeMount() {
+    const apiUrl = 'http://localhost:3000';
+    Vue.prototype.$apiUrl = apiUrl;
+  },
+  render: h => h(App),
+}).$mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="conteiner">
+    <div class="login-wrapper">
+      <div class="d-flex justify-content-center">
+        <img src="../assets/Logo_DR.png" class="img-fluid w-50" />
+      </div>
+      <div class="hold-login mt-4 p-4 rounded-lg bg-white shadow-sm">
+        <div class="login-text d-flex justify-content-center mb-4">
+          <h4>Login</h4>
+        </div>
+
+        <div>
+          <advanced-input @value="inputValue($event, 'cracha')" :label="'Crachá:'" :type="'text'" />
+        </div>
+        <div>
+          <advanced-input @value="inputValue($event, 'senha')" :label="'Senha:'" :type="'password'" />
+        </div>
+      </div>
+      <div class="d-flex justify-content-center m-3">
+        <b-button @click="loginValidation" variant="danger">Acessar</b-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import advancedInput from '../components/inputs/advanced-input'
+
+export default {
+  components: {
+    'advanced-input': advancedInput,
+  },
+
+  data() {
+    return {
+      loginVModel: [
+        { type: 'cracha', value: '' },
+        { type: 'senha', value: '' }
+      ],
+    };
+  },
+
+  methods: {
+    inputValue($event, model) {
+      this.loginVModel.forEach(vmodel => {
+        if (vmodel.type === model) vmodel.value = $event
+      });
+    },
+
+    loginValidation() {
+      // usar metodo fetch para as requições, não precisa instalar
+      // nenhuma dependencia e é mais leve por não usar nenhum npm install
+    },
+  },
+};
+
+</script>
+
+<style lang="scss">
+.conteiner {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-wrapper {
+  min-width: 20vw;
+  min-height: 100px;
+  padding: 10px;
+}
+
+@media screen and (max-width: 1366px) {
+  .login-wrapper {
+    min-width: 30vw;
+  }
+}
+
+.login-text {
+  h4 {
+    color: #777777;
+  }
+  border-bottom: 1px solid #e4e4e4;
+  width: 100%;
+}
+</style>
