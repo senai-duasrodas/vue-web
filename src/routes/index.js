fb5dd6042eb713bbcd428271c7659bbfb55550ff
douglasblnk <douglasblank1@gmail.com>
2019-11-06 00:30:54 -0300

    Changed the auth pages to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,50 +4,81 @@ import Dashboard from '../views/Dashboard'
 import Cadastros from '../views/Cadastros'
 import Login from '../views/Login'
 
+import validate from '../utils/user-validation';
+
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/login',
+    path: '/',
     name: 'login',
     component: Login
   },
   {
     path: '/dashboard',
     name: 'dashboard',
-    component: Dashboard
+    component: Dashboard,
+    meta: { requireAuth: true }
   },
   {
     path: '/cadastros',
     name: 'cadastros',
-    component: Cadastros
+    component: Cadastros,
+    meta: { requireAuth: true }
   },
   {
     path: '/solicitacoes',
     name: 'Solicitações',
-    component: Dashboard
+    component: Dashboard,
+    meta: { requireAuth: true }
   },
   {
     path: '/consultas',
     name: 'Consultas',
-    component: Dashboard
+    component: Dashboard,
+    meta: { requireAuth: true }
   },
   {
     path: '/relatorios',
     name: 'Relatórios',
-    component: Dashboard
+    component: Dashboard,
+    meta: { requireAuth: true }
   },
   {
     path: '/configuracoes',
     name: 'Configurações',
-    component: Dashboard
+    component: Dashboard,
+    meta: { requireAuth: true }
   }
 ]
+
+const apiUrl = 'http://localhost:3000';
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  apiUrl
 })
+
+router.beforeEach(async (to, from, next) => {
+  if (to.meta.requireAuth) {
+    try {
+      const response = await validate.validate(this.$apiUrl)
+
+      console.log('sucess: ', response);
+      next();
+    } catch (err) {
+      console.log('error: ', err);
+
+      localStorage.removeItem('token');
+
+      if (router.name === 'login') return
+      router.replace('');
+      next();
+      
+    }
+  } else next();
+});
 
 export default router
